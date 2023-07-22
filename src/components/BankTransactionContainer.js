28807f5819a6ab4e0191666e0aef07a95dbaa06a
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-07-22 23:31:10 +0300

    Update BankTransactionContainer.js

--- src/components/BankTransactionContainer.js
+++ src/components/BankTransactionContainer.js
@@ -1,1 +1,34 @@
+import React, {useEffect, useState} from "react";
+import Search from "./Searchbar";
+import TransactionList from "./TransactionList";
+import TransactionForm from "./TransactionForm";
+
+function BankTransactionContainer(){
+    //State to store th transactions that are fetched from the API.
+    const [transactions, setTransactions] = useState([]); 
+
+   //State to manage the search query for the filtering of transactions.
+    const [query, setQuery] = useState("");
+    useEffect(() => {
+     // Fetch the transactions from the API.
+      fetch("http://localhost:8001/transactions?q=" +query)  
+      .then((resp) =>resp.json())
+      .then((data) =>setTransactions(data))
+      .catch((error) => console.error("Error fetching transactions:", error));
+    },[query]);
+     
+    // The Handle function is to update the search query state
+    function handleSearch(e) {
+        setQuery(e.target.value);
+    }
+    return(
+        <div>
+            <Search handleSearch={handleSearch} />
+            <TransactionForm />
+            <TransactionList transations={transaction} />
+
+        </div>
+    );
+    }
+    export default BankTransactionContainer;
 
