0b8734dfce3aed48f1835199d8b84e15bc74608f
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-07-22 22:38:08 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,14 @@
+import REACT from "react";
+import BankTransactionContainer from "./BankTransaction";
 
+function App() {
+    return(
+        <div className="ui raised segment">
+        <div className="ui segment violet inverted">
+         <h2>The Royal Bank of Flatiron</h2>
+         </div>   
+         <BankTransaction />
+        </div>
+    )
+}
+export default App;
