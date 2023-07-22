4d23343ae26288d2707211037636bf2f0eb2532c
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-07-22 19:50:44 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import REACTDOM from "react-dom";
+import App from "./components/App";
+import "./index.css";
+import "semintic-ui-css/semantic.min.css";
+
+ReactDOM.render(
+<App />,
+document.getElementById("root")
+);
