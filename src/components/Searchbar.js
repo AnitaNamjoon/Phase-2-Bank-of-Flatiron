8a1b04132723b9a02aa63fb894090d1a0541ac1d
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-07-22 23:52:06 +0300

    Update Searchbar.js

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,1 +1,24 @@
+import React from "react";
 
+function Search({handleSearch}) {
+const [searchText, setSearchText] = useState("");
+
+const handleChange = (e) => {
+    const searchText = e.target.value;
+    setSearchText(searchText);
+    handleSearch(searchText); 
+  };
+  return (
+    <div className="ui large fluid icon input">
+      <input
+        type="text"
+        placeholder="Search your Recent Transactions"
+        value={searchText}
+        onChange={handleSearch}
+      />
+      <i className="circular search link icon"></i>
+    </div>
+  );
+}
+
+export default Search;
