34d9be80249e748a8d6ff1c00b77530dd404572c
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-07-27 20:27:57 +0300

    Create BankTable.js

---
+++ src/BankTable.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Transaction from "./Transaction";
+
+const Table = ({ transactions, onDelete, onSort }) => {
+  const handleDelete = ({ id }) => {
+    onDelete(id);
+  };
+
+  const handleSort = (field) => {
+    onSort(field);
+  };
+
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th onClick={() => handleSort("date")}>Date</th>
+          <th onClick={() => handleSort("description")}>Description</th>
+          <th onClick={() => handleSort("category")}>Category</th>
+          <th onClick={() => handleSort("amount")}>Amount</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map((transaction) => (
+          <Transaction
+            key={transaction.id}
+            transaction={transaction}
+            onDelete={() => handleDelete({ id: transaction.id })}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
