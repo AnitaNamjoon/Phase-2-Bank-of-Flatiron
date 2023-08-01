a2243c2b56c1a4a850eb88566ba11a1c36b2602f
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-08-02 00:34:19 +0300

    Create NavigationBar.js

---
+++ src/NavigationBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar =() => {
+    return (
+        <nav className="navbar bg-primary">
+        <div className="container-fluid d-flex justify-content-center">
+          <h2 className="navbar-brand text-white">The Royal Bank of Flatiron</h2>
+        </div>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
