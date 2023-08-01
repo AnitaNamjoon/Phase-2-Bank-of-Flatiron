fdffa7f6ebf30c6d6e722ed5176264c297329d9b
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-08-02 00:35:56 +0300

    Create Transaction.js

---
+++ src/Transaction.js
@@ -0,0 +1,19 @@
+import React from "react";
+const Transaction = ({ transaction, onDelete }) => {
+    const { id, date, description, category, amount } = transaction;
+    return (
+        <tr>
+            <td>{date}</td>
+            <td>{description}</td>
+            <td>{category}</td>
+            <td>{amount}</td>
+            <button
+                onClick={() => onDelete(id)}
+                className="btn btn-danger"
+            >
+                Delete
+            </button>
+        </tr>
+    );
+}
+export default Transaction;
